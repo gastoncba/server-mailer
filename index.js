beb9a7f792ed241cb1fa839a17e9234d30da9a03
Gaston Barrionuevo <gaston.cba97@gmail.com>
2022-03-02 17:01:18 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+//const routerApi = require('./router')
+const bodyParser = require('body-parser')
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+const router = express.Router();
+
+app.use(cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: "*",
+    exposedHeaders: "*",
+    credentials: true
+}));
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use("/", router);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+//routers
+router.get('/', (req, res) => {res.send('TEST Email JS')})
+//routerApi(app)
